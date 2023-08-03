4a585d1527b5bb8d3831afc9a50d2290c295f552
Kristie Patterson <kpatterson395@gmail.com>
2023-08-02 20:02:01 -0400

    ability to edit/delete user

--- src/EditUser.js
+++ src/EditUser.js
@@ -1,13 +1,70 @@
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-// import { editUser, deleteUser } from "./redux/userData";
+import TextField from "@mui/material/TextField";
+import { useState } from "react";
+import { editUser } from "./redux/userData";
 
 const EditUser = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { users } = useSelector((state) => state.userData);
   let user = users.find((x) => x.id === id);
-  return <div>{user.first}</div>;
+
+  const [first, setFirst] = useState(user.first);
+  const [last, setLast] = useState(user.last);
+  const [phone, setPhone] = useState(user.phone);
+  const [email, setEmail] = useState(user.email);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      editUser({
+        id,
+        first,
+        last,
+        phone,
+        email,
+      })
+    );
+    navigate(`/users/${id}`);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <TextField
+          required
+          id="outlined-required"
+          label="first name"
+          value={first}
+          onChange={(e) => setFirst(e.target.value)}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="last name"
+          value={last}
+          onChange={(e) => setLast(e.target.value)}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="phone number"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+        />
+        <TextField
+          required
+          id="outlined-required"
+          label="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button>Submit Changes</button>
+      </form>
+    </div>
+  );
 };
 
 export default EditUser;
--- src/UserDetails.js
+++ src/UserDetails.js
@@ -1,12 +1,19 @@
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { editUser, deleteUser } from "./redux/userData";
 
 const UserDetails = () => {
   const { id } = useParams();
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { users } = useSelector((state) => state.userData);
   let user = users.find((x) => x.id === id);
+
+  const handleDelete = () => {
+    dispatch(deleteUser(id));
+    navigate("/users");
+  };
+
   return (
     <div>
       <h2>User Details:</h2>
@@ -18,7 +25,7 @@ const UserDetails = () => {
       <button>
         <Link to={`/users/edit/${user.id}`}>Edit User</Link>
       </button>
-      <button onClick={() => dispatch(deleteUser(id))}>Delete User</button>
+      <button onClick={handleDelete}>Delete User</button>
     </div>
   );
 };
