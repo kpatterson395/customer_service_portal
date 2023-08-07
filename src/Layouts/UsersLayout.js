23c6de54856f928db67e3c478771d2ceab4c7b8b
Kristie Patterson <kpatterson395@gmail.com>
2023-08-07 07:00:41 -0400

    tab err fix

--- src/Layouts/UsersLayout.js
+++ src/Layouts/UsersLayout.js
@@ -28,7 +28,7 @@ export default function UsersLayout() {
     <>
       <Box sx={{ width: "100%" }}>
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <Tabs aria-label="basic tabs example" value={pathname}>
+          <Tabs aria-label="user navigation tabs">
             <Tab
               label="Account Details"
               component={NavLink}
